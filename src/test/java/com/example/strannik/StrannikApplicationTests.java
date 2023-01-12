f83b5ea13ce628e229eb9864cbaafdb4c5192ee5
Fedor Ivanov <kenola82007@gmail.com>
2023-01-12 12:27:14 +0300

    Initial commit

---
+++ src/test/java/com/example/strannik/StrannikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strannik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrannikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
